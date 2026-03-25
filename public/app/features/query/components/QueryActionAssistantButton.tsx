941ffab6cc90eec58e3e2524cdf5aa9e0bf4264a
k-munoz <karina.munozvillanueva@grafana.com>
2026-03-25 16:43:07 -0400

    updated placement of button

--- public/app/features/query/components/QueryActionAssistantButton.tsx
+++ public/app/features/query/components/QueryActionAssistantButton.tsx
@@ -37,9 +37,9 @@ export function QueryActionAssistantButton<TQuery extends DataQuery = DataQuery>
     return null;
   }
 
-  // Only show for loki and prometheus datasources
+  // Prometheus and loki both have their own buttons inside their query editors
   const pluginId = dataSourceInstanceSettings.type;
-  if (pluginId !== 'loki' && pluginId !== 'prometheus') {
+  if (pluginId === 'loki' || pluginId === 'prometheus') {
     return null;
   }
   const origin = `grafana/query-editor/${pluginId}/${app ?? CoreApp.Unknown}`;
